36bff61c5aa8b6858dd8b6bb74bba13280bf97f5
Gabriel Pasquarelli <gapasquarelli@Gabriels-MacBook-Pro-2.local>
2023-08-23 01:55:04 -0300

    Adicionando mais intents

--- LAB01/main.py
+++ LAB01/main.py
@@ -8,18 +8,17 @@ myChatBot.createModel()
 
 
 
-
 print("Bem vindo ao Chatbot")
 
 
-pergunta = input("como posso te ajudar?")
+pergunta = input("como posso te ajudar?\n")
 resposta, intencao = myChatBot.chatbot_response(pergunta)
 print(resposta + "   ["+intencao[0]['intent']+"]")
 
 
 while (intencao[0]['intent']!="despedida"):
-    pergunta = input("posso lhe ajudar com algo a mais?")
+    pergunta = input("posso lhe ajudar com algo a mais?\n")
     resposta, intencao = myChatBot.chatbot_response(pergunta)
     print(resposta + "   [" + intencao[0]['intent'] + "]")
 
-print("Foi um prazer atendê-lo")
+print("Foi um prazer atendê-lo!")
